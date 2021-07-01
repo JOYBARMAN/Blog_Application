561e86040aecfff01503b3619fd3aef024b41c1b
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2021-07-01 13:22:32 +0600

    Working with add blog post

--- Blog/myblog/urls.py
+++ Blog/myblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from . views import HomeView,ArticaleDetailView
+from . views import HomeView,ArticaleDetailView,AddPostView
 
 urlpatterns = [
     path('',HomeView.as_view(),name="home"),
     path('articale/<int:pk>',ArticaleDetailView.as_view(),name="articale-detail"),
+    path('add_post/',AddPostView.as_view(),name="add_post")
 ]
--- Blog/myblog/views.py
+++ Blog/myblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Post
 
 
@@ -10,3 +10,9 @@ class HomeView(ListView):
 class ArticaleDetailView(DetailView):
 	model=Post
 	template_name='articale_details.html'
+
+class AddPostView(CreateView):
+	model=Post
+	template_name='add_post.html'
+	fields='__all__'
+	# fields=('title','body')
