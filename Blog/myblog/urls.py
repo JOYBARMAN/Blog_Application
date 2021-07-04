e7a2e75377882b03eb888217fc1c710077c59214
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2021-07-04 14:10:42 +0600

    add bootstrap and update section

--- Blog/myblog/urls.py
+++ Blog/myblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from . views import HomeView,ArticaleDetailView,AddPostView
+from . views import HomeView,ArticaleDetailView,AddPostView,UpdatePostView
 
 urlpatterns = [
     path('',HomeView.as_view(),name="home"),
     path('articale/<int:pk>',ArticaleDetailView.as_view(),name="articale-detail"),
-    path('add_post/',AddPostView.as_view(),name="add_post")
+    path('add_post/',AddPostView.as_view(),name="add_post"),
+    path('articale/edit/<int:pk>',UpdatePostView.as_view(),name="update_post"),
 ]
--- Blog/myblog/views.py
+++ Blog/myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Post
+from .forms import PostForm,UpdateForm
 
 
 class HomeView(ListView):
@@ -13,6 +14,14 @@ class ArticaleDetailView(DetailView):
 
 class AddPostView(CreateView):
 	model=Post
+	form_class=PostForm
 	template_name='add_post.html'
-	fields='__all__'
+	# fields='__all__'
 	# fields=('title','body')
+
+class UpdatePostView(UpdateView):
+	model=Post
+	form_class=UpdateForm
+	template_name='update_post.html'
+	# fields=('title','body')
+
